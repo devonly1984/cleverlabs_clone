e50ea3f6702f1c53af23aa4de177465d1015e4a8
pcguard <pcguard@PON2027493D.safeway01.ad.safeway.com>
2026-03-01 06:35:06 -0600

    First Commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+
+import "./globals.css";
+import { ReactNode } from "react";
+import { inter } from "@/constants/fonts";
+import { Toaster } from "@/components/ui/sonner";
+import {ClerkProvider} from '@clerk/nextjs'
+
+
+export const metadata: Metadata = {
+  title: "Eleven Labs Clone",
+  description: "Eleven Labs Tut by CWA",
+};
+
+const RootLayout = ({
+  children,
+}: Readonly<{
+  children: ReactNode;
+}>) => {
+  return (
+    <ClerkProvider>
+    <html lang="en">
+      <body className={` ${inter.variable}antialiased`}>
+        {children}
+        <Toaster />
+      </body>
+    </html>
+    </ClerkProvider>
+  );
+};
+export default RootLayout;
